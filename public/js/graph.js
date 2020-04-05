28c85a2d3d44fcf94ab6e7db43e091a525a26360
Joshua Corpin <jkcorpin@gmail.com>
2020-04-05 15:35:46 +0800

    Removed LOGS

--- public/js/graph.js
+++ public/js/graph.js
@@ -118,7 +118,7 @@ export const showColumnGraph = async () => {
 
   var chart = new CanvasJS.Chart('chartContainer2', {
     animationEnabled: true,
-    theme: 'light2', // "light1", "light2", "dark1", "dark2"
+    theme: 'light2', // "light1", "dark1", "dark2"
     title: {
       text: 'Daily New Cases',
     },
